3fe1aba3091b2bb3acc3a5aff81774cffb9b1b05
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-03-24 09:47:03 +0200

    fix semicolons

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -2,23 +2,23 @@
 SELECT *
 FROM cities
 ORDER BY population ASC
-LIMIT 10 OFFSET 10
+LIMIT 10 OFFSET 10;
 
 --Отримати список міст відсортованим за назвою в зворотньому порядку (від Я до А). Обмежити запит 30 запасами
 SELECT * 
 FROM cities
 ORDER BY name DESC
-LIMIT 30
+LIMIT 30;
 
 --Отримати список міст відсортованих за регіоном та кількістю населення (спочатку найнаселеніші міста регіону)
 SELECT * 
-FROM cities ORDER BY region ASC, population DESC
+FROM cities ORDER BY region ASC, population DESC;
 
 --Отримати список регіонів вказаних в таблиці
 SELECT DISTINCT region
-FROM cities
+FROM cities;
 
 --Отримати список міст відсортованих за регіоном у зворотньому порядку та за назвою у межах регіону також у зворотньому порядку
 SELECT * 
 FROM cities 
-ORDER BY region DESC, name DESC
+ORDER BY region DESC, name DESC;
--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,29 +1,29 @@
 --Отримати список міст-мільйонерів України
 SELECT *
 FROM cities
-WHERE population >= 1000000
+WHERE population >= 1000000;
 
 --Отримати список міст з регіону E та W відсортований за кількістю населення
 SELECT * 
 FROM cities
 WHERE region = 'E' OR 'W'
-ORDER BY population ASC
+ORDER BY population ASC;
 
 --Отримати список міст з населенням більше ніж 50000 з регіонів S, C та N (вікористовуючи оператор IN)
 SELECT * 
 FROM cities
-WHERE population >= 50000 AND region IN('S', 'C', 'N')
+WHERE population >= 50000 AND region IN('S', 'C', 'N');
 
 --Отримати перші 20 міст (за назвою) у яких населення більше ніж 150000 але не більше ніж 350000 та які знаходяться в регіонах E,W,N
 SELECT * 
 FROM cities
 WHERE population BETWEEN 150000 AND 350000 AND region IN('E', 'W', 'N')
 ORDER BY name ASC
-LIMIT 20
+LIMIT 20;
 
 --Отримати другу десятку міст за кількістю населення які не входять до регіонів E та W
 SELECT * 
 FROM cities 
 WHERE region NOT IN ('E', 'W')
 ORDER BY population ASC
-LIMIT 10 OFFSET 10
+LIMIT 10 OFFSET 10;
