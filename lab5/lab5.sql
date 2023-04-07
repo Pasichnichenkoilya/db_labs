390c3e115bfc2d3d5d70069e179832034b78add5
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-04-07 11:06:50 +0300

    Update lab5.sql

--- lab5/lab5.sql
+++ lab5/lab5.sql
@@ -35,4 +35,4 @@ SELECT name, population
 FROM cities
 WHERE population NOT BETWEEN 150000 AND 500000 AND region IN (SELECT uuid
                   FROM regions
-                  WHERE area_quantity <= 5)
+                  WHERE area_quantity <= 5);
